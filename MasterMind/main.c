139548b8e265c44a0328de9255f1cc52ebd71b90
Bernardobvb <50021260+Bernardobvb@users.noreply.github.com>
2021-03-14 11:16:04 -0300

    updtade

--- MasterMind/main.c
+++ MasterMind/main.c
@@ -64,6 +64,21 @@ int main()
     ALLEGRO_COLOR verde = al_map_rgb(20, 240, 100);
     ALLEGRO_COLOR cinza = al_map_rgb(128, 128, 128);
 
+    ALLEGRO_COLOR matriz[7][4];
+    int i;
+    int j;
+    for(i=0; i<7; i++){
+        for(j=0; j<4; j++){
+            matriz[i][j] = vermelho;
+        }
+    }
+
+    ALLEGRO_COLOR matrizResposta [7][4];
+    for(i=0; i<7; i++){
+        for(j=0; j<4; j++){
+            matrizResposta[i][j] = cinza;
+        }
+    }
     al_start_timer(timer);
     while(1)
     {
@@ -94,93 +109,93 @@ int main()
             //al_draw_bitmap(mysha, 100, 100, 0);
             //Primeiraa Fileira
             al_draw_filled_rectangle(600,50,700,100, verde);
-            al_draw_filled_circle(100, 75, 40, amarelo);
-            al_draw_filled_circle(230, 75, 40, branco);
-            al_draw_filled_circle(360, 75, 40, branco);
-            al_draw_filled_circle(490, 75, 40, branco);
+            al_draw_filled_circle(100, 75, 40, matriz[0][0]);
+            al_draw_filled_circle(230, 75, 40, matriz[0][1]);
+            al_draw_filled_circle(360, 75, 40, matriz[0][2]);
+            al_draw_filled_circle(490, 75, 40, matriz[0][3]);
             //Segunda Fileira
             al_draw_filled_rectangle(600,150,700,200, verde);
-            al_draw_filled_circle(100, 175, 40, branco);
-            al_draw_filled_circle(230, 175, 40, branco);
-            al_draw_filled_circle(360, 175, 40, branco);
-            al_draw_filled_circle(490, 175, 40, branco);
+            al_draw_filled_circle(100, 175, 40, matriz[1][0]);
+            al_draw_filled_circle(230, 175, 40, matriz[1][1]);
+            al_draw_filled_circle(360, 175, 40, matriz[1][2]);
+            al_draw_filled_circle(490, 175, 40, matriz[1][3]);
             //Terceira Fileira
             al_draw_filled_rectangle(600,250,700,300, verde);
-            al_draw_filled_circle(100, 275, 40, branco);
-            al_draw_filled_circle(230, 275, 40, branco);
-            al_draw_filled_circle(360, 275, 40, branco);
-            al_draw_filled_circle(490, 275, 40, branco);
+            al_draw_filled_circle(100, 275, 40, matriz[2][0]);
+            al_draw_filled_circle(230, 275, 40, matriz[2][1]);
+            al_draw_filled_circle(360, 275, 40, matriz[2][2]);
+            al_draw_filled_circle(490, 275, 40, matriz[2][3]);
             //Quarta Fileira
             al_draw_filled_rectangle(600,350,700,400, verde);
-            al_draw_filled_circle(100, 375, 40, branco);
-            al_draw_filled_circle(230, 375, 40, branco);
-            al_draw_filled_circle(360, 375, 40, branco);
-            al_draw_filled_circle(490, 375, 40, branco);
+            al_draw_filled_circle(100, 375, 40, matriz[3][0]);
+            al_draw_filled_circle(230, 375, 40, matriz[3][1]);
+            al_draw_filled_circle(360, 375, 40, matriz[3][2]);
+            al_draw_filled_circle(490, 375, 40, matriz[3][3]);
              //Quinta Fileira
             al_draw_filled_rectangle(600,450,700,500, verde);
-            al_draw_filled_circle(100, 475, 40, branco);
-            al_draw_filled_circle(230, 475, 40, branco);
-            al_draw_filled_circle(360, 475, 40, branco);
-            al_draw_filled_circle(490, 475, 40, branco);
+            al_draw_filled_circle(100, 475, 40, matriz[4][0]);
+            al_draw_filled_circle(230, 475, 40, matriz[4][1]);
+            al_draw_filled_circle(360, 475, 40, matriz[4][2]);
+            al_draw_filled_circle(490, 475, 40, matriz[4][3]);
             //Sexta Fileira
             al_draw_filled_rectangle(600,550,700,600, verde);
-            al_draw_filled_circle(100, 575, 40, branco);
-            al_draw_filled_circle(230, 575, 40, branco);
-            al_draw_filled_circle(360, 575, 40, branco);
-            al_draw_filled_circle(490, 575, 40, branco);
+            al_draw_filled_circle(100, 575, 40, matriz[5][0]);
+            al_draw_filled_circle(230, 575, 40, matriz[5][1]);
+            al_draw_filled_circle(360, 575, 40, matriz[5][2]);
+            al_draw_filled_circle(490, 575, 40, matriz[5][3]);
             //Sétima Fileira
             al_draw_filled_rectangle(600,650,700,700, verde);
-            al_draw_filled_circle(100, 675, 40, branco);
-            al_draw_filled_circle(230, 675, 40, branco);
-            al_draw_filled_circle(360, 675, 40, branco);
-            al_draw_filled_circle(490, 675, 40, branco);
+            al_draw_filled_circle(100, 675, 40, matriz[6][0]);
+            al_draw_filled_circle(230, 675, 40, matriz[6][1]);
+            al_draw_filled_circle(360, 675, 40, matriz[6][2]);
+            al_draw_filled_circle(490, 675, 40, matriz[6][3]);
 
             //Respostas Bolas brancas e vermelhas
 
-            al_draw_filled_circle(750,55,15, cinza);
-            al_draw_filled_circle(790,55,15, cinza);
-            al_draw_filled_circle(750,95,15, cinza);
-            al_draw_filled_circle(790,95,15, cinza);
+            al_draw_filled_circle(750,55,15, matrizResposta[0][0]);
+            al_draw_filled_circle(790,55,15, matrizResposta[0][1]);
+            al_draw_filled_circle(750,95,15, matrizResposta[0][2]);
+            al_draw_filled_circle(790,95,15, matrizResposta[0][3]);
 
-            al_draw_filled_circle(750,155,15, cinza);
-            al_draw_filled_circle(790,155,15, cinza);
-            al_draw_filled_circle(750,195,15, cinza);
-            al_draw_filled_circle(790,195,15, cinza);
+            al_draw_filled_circle(750,155,15, matrizResposta[1][0]);
+            al_draw_filled_circle(790,155,15, matrizResposta[1][1]);
+            al_draw_filled_circle(750,195,15, matrizResposta[1][2]);
+            al_draw_filled_circle(790,195,15, matrizResposta[1][3]);
 
-            al_draw_filled_circle(750,255,15, cinza);
-            al_draw_filled_circle(790,255,15, cinza);
-            al_draw_filled_circle(750,295,15, cinza);
-            al_draw_filled_circle(790,295,15, cinza);
+            al_draw_filled_circle(750,255,15, matrizResposta[2][0]);
+            al_draw_filled_circle(790,255,15, matrizResposta[2][1]);
+            al_draw_filled_circle(750,295,15, matrizResposta[2][2]);
+            al_draw_filled_circle(790,295,15, matrizResposta[2][3]);
 
-            al_draw_filled_circle(750,355,15, cinza);
-            al_draw_filled_circle(790,355,15, cinza);
-            al_draw_filled_circle(750,395,15, cinza);
-            al_draw_filled_circle(790,395,15, cinza);
+            al_draw_filled_circle(750,355,15, matrizResposta[3][0]);
+            al_draw_filled_circle(790,355,15, matrizResposta[3][1]);
+            al_draw_filled_circle(750,395,15, matrizResposta[3][2]);
+            al_draw_filled_circle(790,395,15, matrizResposta[3][3]);
 
-            al_draw_filled_circle(750,455,15, cinza);
-            al_draw_filled_circle(790,455,15, cinza);
-            al_draw_filled_circle(750,495,15, cinza);
-            al_draw_filled_circle(790,495,15, cinza);
+            al_draw_filled_circle(750,455,15, matrizResposta[4][0]);
+            al_draw_filled_circle(790,455,15, matrizResposta[4][1]);
+            al_draw_filled_circle(750,495,15, matrizResposta[4][2]);
+            al_draw_filled_circle(790,495,15, matrizResposta[4][3]);
 
-            al_draw_filled_circle(750,555,15, al_map_rgba_f(1,1,1,1));
-            al_draw_filled_circle(790,555,15, al_map_rgba_f(1,1,1,1));
-            al_draw_filled_circle(750,595,15, al_map_rgba_f(1,1,1,1));
-            al_draw_filled_circle(790,595,15, al_map_rgba_f(1,1,1,1));
+            al_draw_filled_circle(750,555,15, matrizResposta[5][0]);
+            al_draw_filled_circle(790,555,15, matrizResposta[5][1]);
+            al_draw_filled_circle(750,595,15, matrizResposta[5][2]);
+            al_draw_filled_circle(790,595,15, matrizResposta[5][3]);
 
-            al_draw_filled_circle(750,655,15, al_map_rgba_f(1,1,1,1));
-            al_draw_filled_circle(790,655,15, al_map_rgba_f(1,1,1,1));
-            al_draw_filled_circle(750,695,15, al_map_rgba_f(1,1,1,1));
-            al_draw_filled_circle(790,695,15, al_map_rgba_f(1,1,1,1));
+            al_draw_filled_circle(750,655,15, matrizResposta[6][0]);
+            al_draw_filled_circle(790,655,15, matrizResposta[6][1]);
+            al_draw_filled_circle(750,695,15, matrizResposta[6][2]);
+            al_draw_filled_circle(790,695,15, matrizResposta[6][3]);
 
             //COLUNA LATERAL
 
-            al_draw_filled_circle(935,100,40, al_map_rgba_f(1,0,0,1));
-            al_draw_filled_circle(935,200,40, al_map_rgba_f(0,1,0,1));
-            al_draw_filled_circle(935,300,40, al_map_rgba_f(0,0,1,1));
-            al_draw_filled_circle(935,400,40, al_map_rgba_f(0,1,1,1));
+            al_draw_filled_circle(935,100,40, vermelho);
+            al_draw_filled_circle(935,200,40, amarelo);
+            al_draw_filled_circle(935,300,40, verde);
+            al_draw_filled_circle(935,400,40, azul);
 
-            al_draw_filled_circle(880,590,15, al_map_rgba_f(1,1,1,1));
-            al_draw_filled_circle(880,650,15, al_map_rgba_f(1,0,0,1));
+            al_draw_filled_circle(880,590,15, branco);
+            al_draw_filled_circle(880,650,15, vermelho);
 
             //Linha
             al_draw_line(850, 10, 850, SCREEN_H-10, al_map_rgb_f(1, 0, 0), 3);
